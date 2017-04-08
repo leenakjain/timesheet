ef42685942c030fd22da5e66b53b441a16770289
Leena Jain <leenakjain@gmail.com>
2017-04-09 00:21:15 +1000

    Added editors

---
+++ src/main/java/org/lnk/timesheet/web/editors/EmployeeEditor.java
@@ -0,0 +1,25 @@
+package org.lnk.timesheet.web.editors;
+
+import org.lnk.timesheet.domain.Employee;
+import org.lnk.timesheet.service.dao.EmployeeDao;
+
+import java.beans.PropertyEditorSupport;
+
+/**
+ * Will convert ID from combobox to employee's instance.
+ */
+public class EmployeeEditor extends PropertyEditorSupport {
+
+    private EmployeeDao employeeDao;
+
+    public EmployeeEditor(EmployeeDao employeeDao) {
+        this.employeeDao = employeeDao;
+    }
+
+    @Override
+    public void setAsText(String text) throws IllegalArgumentException {
+        long id = Long.parseLong(text);
+        Employee employee = employeeDao.find(id);
+        setValue(employee);
+    }
+}
---
+++ src/main/java/org/lnk/timesheet/web/editors/ManagerEditor.java
@@ -0,0 +1,25 @@
+package org.lnk.timesheet.web.editors;
+
+import org.lnk.timesheet.domain.Manager;
+import org.lnk.timesheet.service.dao.ManagerDao;
+
+import java.beans.PropertyEditorSupport;
+
+/**
+ * Will convert ID from combobox to manager's instance
+ */
+public class ManagerEditor extends PropertyEditorSupport {
+
+    private ManagerDao managerDao;
+
+    public ManagerEditor(ManagerDao managerDao) {
+        this.managerDao = managerDao;
+    }
+
+    @Override
+    public void setAsText(String text) throws IllegalArgumentException {
+        long id = Long.parseLong(text);
+        Manager manager = managerDao.find(id);
+        setValue(manager);
+    }
+}
